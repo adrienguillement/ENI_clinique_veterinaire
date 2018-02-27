a99dd71c3b037a6db3994466bf22da72fba8bad7
Ludo <kiluhasenpai@gmail.com>
2018-02-27 16:52:37 +0100

    en attend le refactoring

--- clinique/src/fr/eni/clinique/bll/PersonnelManager.java
+++ clinique/src/fr/eni/clinique/bll/PersonnelManager.java
@@ -1,4 +1,33 @@
 package src.fr.eni.clinique.bll;
 
+import src.fr.eni.clinique.bo.Personne;
+import src.fr.eni.clinique.dal.DALException;
+import src.fr.eni.clinique.dal.DAOFactory;
+import src.fr.eni.clinique.dal.DAOPersonnels;
+
+import java.util.List;
+
 public class PersonnelManager {
+    private static DAOPersonnels daoPersonnels;
+
+    public PersonnelManager() throws BLLException{
+        daoPersonnels = DAOFactory.getPersonnelsDAO();
+    }
+
+    /**
+     *
+     * @return
+     * @throws BLLException
+     */
+    public List<Personne> getPersonnels() throws BLLException{
+        List<Personne> personne = null;
+
+        try{
+            personne = DAOFactory.getPersonnelsDAO().selectAll();
+        }catch(DALException e){
+            e.printStackTrace();
+            throw new BLLException("Erreur récupération du personnels", e);
+        }
+        return personne;
+    }
 }
--- clinique/src/fr/eni/clinique/ihm/GestionPersonnelController.java
+++ clinique/src/fr/eni/clinique/ihm/GestionPersonnelController.java
@@ -1,4 +1,32 @@
+package src.fr.eni.clinique.ihm;
+
+import src.fr.eni.clinique.bll.BLLException;
+import src.fr.eni.clinique.bll.PersonnelManager;
+import src.fr.eni.clinique.bo.Personne;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
 public class GestionPersonnelController
 {
+    private static IHMApp ecran;
+    private static GestionPersonnelController instance;
+    private PersonnelManager personnelManager = new PersonnelManager();
+    private GeneralController controllerBase = GeneralController.getInstance();
 
+    // constructeur
+    private GestionPersonnelController() throws BLLException {
+        getEcran();
+    }
+
+    // gestion de l'IHM
+    public IHMApp getEcran() {
+        if(ecran == null){
+            ecran = IHMApp.getInstance();
+        }
+        return ecran;
+    }
 }
