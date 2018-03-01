c3d600a28e7422b4e0b87f0e5ae08b7bc2df2a63
AlexMono <35962752+AlexMono@users.noreply.github.com>
2018-03-01 10:33:58 +0100

    IHMAnimal tableau dynamique

--- clinique/src/fr/eni/clinique/ihm/IHMAnimal.java
+++ clinique/src/fr/eni/clinique/ihm/IHMAnimal.java
@@ -6,6 +6,9 @@ import fr.eni.clinique.bll.RaceManager;
 import fr.eni.clinique.bo.Animal;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.util.List;
 
@@ -15,6 +18,7 @@ public class IHMAnimal extends JFrame{
     private RaceManager raceManager;
     //private ModeleDynamiqueObjetAnimal modele = new ModeleDynamiqueObjetAnimal();
     private JTable table;
+    private Animal animalSelected;
 
     // Lancement de l'application
     public static void main(String[] args) {
@@ -39,9 +43,9 @@ public class IHMAnimal extends JFrame{
             e.printStackTrace();
         }
 
-        List<Animal> animaux = animalManager.getListeAnimaux();
+        //List<Animal> animaux = animalManager.getListeAnimaux();
 
-        setIHMAnimal(animaux);
+        setIHMAnimal(null);
     }
 
     private void setIHMAnimal(List<Animal> Animaux) {
@@ -51,44 +55,47 @@ public class IHMAnimal extends JFrame{
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(true);
 
-        table = new JTable();
+        //variables locales ca pas de co bdd atm
+        String[] columns = new String[] {
+                "Code", "Nom", "Sexe", "Couleur", "Race", "Espece"
+        };
 
-        getContentPane().add(new JScrollPane(table), BorderLayout.CENTER);
+        //actual data for the table in a 2d array
+        Object[][] data = new Object[][] {
+                {1, "Ludo", "M", "Bleu", "Labrador", "Chien"},
+                {21, "Jack Souslo", "F", "Violet", "Poisson papillon", "Poisson"},
+        };
+        table = new JTable(data, columns);
+        //add the table to the frame
+        this.add(new JScrollPane(table));
 
-        JPanel boutons = new JPanel();
+        table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 
-        /*boutons.add(new JButton(new AddAction()));
-        boutons.add(new JButton(new RemoveAction()));*/
+        ListSelectionModel cellSelectionModel = table.getSelectionModel();
+        cellSelectionModel.addListSelectionListener(new ListSelectionListener() {
+            public void valueChanged(ListSelectionEvent e) {
+                String selectedData = null;
 
-        getContentPane().add(boutons, BorderLayout.SOUTH);
+                int[] selectedRow = table.getSelectedRows();
+                int[] selectedColumns = table.getSelectedColumns();
 
-        pack();
-
-        //TODO
-    }
-
-    /*private class AddAction extends AbstractAction {
-        private AddAction() {
-            super("Ajouter");
-        }
-
-        public void actionPerformed(ActionEvent e) {
-            modele.addAmi(new Ami("Megan", "Sami", Color.green, false, Sport.NATATION));
-        }
-    }
-
-    private class RemoveAction extends AbstractAction {
-        private RemoveAction() {
-            super("Supprimmer");
-        }
-
-        public void actionPerformed(ActionEvent e) {
-            int[] selection = tableau.getSelectedRows();
-
-            for(int i = selection.length - 1; i >= 0; i--){
-                modele.removeAmi(selection[i]);
+                for (int i = 0; i < selectedRow.length; i++) {
+                    for (int j = 0; j < selectedColumns.length; j++) {
+                        selectedData = (String) table.getValueAt(selectedRow[i], selectedColumns[j]);
+                    }
+                }
+                System.out.println("avant cast");
+                //String selectedValue = ;
+                System.out.println("second cast");
+                int codeAnimal = (int)(table.getValueAt(table.getSelectedRow(), 0));
+                if(codeAnimal != 0){
+                    animalSelected.setCodeAnimal(codeAnimal);
+                    System.out.printf(animalSelected.toString());
+                }
+                //System.out.println("Selected: " + selectedData);
             }
-        }
-    }*/
+
+        });
+    }
 }
 
