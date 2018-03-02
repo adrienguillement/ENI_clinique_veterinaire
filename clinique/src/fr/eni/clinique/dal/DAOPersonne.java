1e82b9aee1344ca873cc09fbbe50cff75ba0cfff
Ludo <kiluhasenpai@gmail.com>
2018-03-02 17:17:43 +0100

    :bath:

--- clinique/src/fr/eni/clinique/dal/DAOPersonne.java
+++ clinique/src/fr/eni/clinique/dal/DAOPersonne.java
@@ -1,4 +1,8 @@
 package fr.eni.clinique.dal;
 
+import fr.eni.clinique.bo.Personnel;
+
 public interface DAOPersonne extends DAO{
+    public void delete(Personnel personnel) throws DALException;
+    public Personnel selectById(int CodePers) throws DALException;
 }
